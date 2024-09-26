a2055170af3a17e75a0c28a958135ede9fb7aa83
Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>
2024-09-26 10:24:19 +0200

    Format file heads to match ICON version.

--- options.meteoswiss/OUTGRID.f
+++ options.meteoswiss/OUTGRID.f
@@ -1,22 +1,15 @@
-!*******************************************************************************
-!                                                                              *
-!      Input file for the Lagrangian particle dispersion model FLEXPART        *
-!                       Please specify your output grid                        *
-!                                                                              *
-! OUTLON0    = GEOGRAPHYICAL LONGITUDE OF LOWER LEFT CORNER OF OUTPUT GRID     *
-! OUTLAT0    = GEOGRAPHYICAL LATITUDE OF LOWER LEFT CORNER OF OUTPUT GRID      *
-! NUMXGRID   = NUMBER OF GRID POINTS IN X DIRECTION (= No. of cells + 1)       *
-! NUMYGRID   = NUMBER OF GRID POINTS IN Y DIRECTION (= No. of cells + 1)       *
-! DXOUT      = GRID DISTANCE IN X DIRECTION                                    *
-! DYOUN      = GRID DISTANCE IN Y DIRECTION                                    *
-! OUTHEIGHTS = HEIGHT OF LEVELS (UPPER BOUNDARY)                               *
-!*******************************************************************************
+!******************************************************************************
+!*                                                                            *
+!*      Input file for the Lagrangian particle dispersion model FLEXPART      *
+!*                       Please specify your output grid                      *
+!*                                                                            *
+!******************************************************************************
 &OUTGRID
- OUTLON0=   -10.000, ! GEOGRAPHYICAL LONGITUDE OF LOWER LEFT CORNER OF OUTPUT GRID
- OUTLAT0=    35.000, ! GEOGRAPHYICAL LATITUDE OF LOWER LEFT CORNER OF OUTPUT GRID
- NUMXGRID=      570, ! NUMBER OF GRID POINTS IN X DIRECTION (= No. of cells + 1)
- NUMYGRID=      300, ! NUMBER OF GRID POINTS IN Y DIRECTION (= No. of cells + 1)
- DXOUT=        0.10, ! GRID DISTANCE IN X DIRECTION
- DYOUT=        0.10, ! GRID DISTANCE IN Y DIRECTION
- OUTHEIGHTS=  500.0, 2000, 10000 ! HEIGHT OF LEVELS (UPPER BOUNDARY)
- /
+ OUTLON0=         -10.000, ! Geographical longitude of lower left corner of output grid
+ OUTLAT0=          35.000, ! Geographical latitude of lower left corner of output grid
+ NUMXGRID=            570, ! Number of grid points in x direction (= No. of cells + 1)
+ NUMYGRID=            300, ! Number of grid points in y direction (= No. of cells + 1)
+ DXOUT=              0.10, ! Grid distance in x direction
+ DYOUT=              0.10, ! Grid distance in y direction
+ OUTHEIGHTS=500,2000,10000 ! Height of levels (upper boundary)
+/
