4adbe16253a34365c999663935bf834fe07940d1
ninaburg <nina.burg@hotmail.com>
2024-12-12 08:56:48 +0100

    update entrpoint and jenkinsfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
 
+# This entrypoint script is the default script run by the Flexpart-IFS container at runtime.
+#
+# The script calls the flexpart_ifs_utils library first to generate the Flexpart input data in the job folder $JOBS_DIR. 
+# This involves configurnig the input namelists (such as COMMAND, AVAILABLE, RELEASES, OUTGRID) based on a set of environment variables, 
+# symlinking the data into the job folder, and writing the job script with the relevent paths to the input files.
+#
+# Then the job files for each release site in runtime_configuration.yaml are run - this runs Flexpart.
+#
+# Finally the flexpart_ifs_utils library is called to upload the output of Flexpart to an S3 bucket.
+
 set -e
 
-workdir=$(pwd)
+# First unset the proxy environment variables if running at AWS or CSCS
+if [ "$DEPLOY_SITE" == "AWS" ] || [ "$DEPLOY_SITE" == "CSCS" ]; then
+    echo "Deploy location is $DEPLOY_SITE. Unsetting proxy environment variables..."
 
-. $SPACK_ROOT/share/spack/setup-env.sh
-spack env activate spack-env
+    unset http_proxy
+    unset https_proxy
+    unset ftp_proxy
+    unset no_proxy
 
-flexpart_prefix=`spack location -i flexpart-ifs`
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
+    unset FTP_PROXY
+    unset NO_PROXY
 
-# Copy the contents of MCH options into to the shared options directory.
-cp -r ${flexpart_prefix}/share/options.meteoswiss/* ${flexpart_prefix}/share/options
+    echo "Proxy environment variables have been unset."
+else
+    echo "Deploy location is not AWS or CSCS. Proxy environment variables remain set."
+fi
 
-./sandbox_generator.py --flexpart_dir ${flexpart_prefix} --sandbox_dir ${workdir}/sandbox;
+# Prepare input files for Flexpart-IFS
+python3.11 -m flexpart_ifs_utils generate \
+    --flexpart_dir $FLEXPART_PREFIX \
+    --jobs_dir $JOBS_DIR \
+    --datetime $FORECAST_DATETIME \
+    --site $RELEASE_SITE_NAME
 
-cd ${workdir}/sandbox
+echo JOBS_DIR: $JOBS_DIR
 
-#Run Flexpart job
-bash job
+# Extract names from generated YAML config
+names=$(grep -oP 'name: \K.*' $JOBS_DIR/runtime_configuration.yaml)
 
-# Upload results to S3 Bucket
+for name in $names; do
+    echo Running Flexpart IFS for release site: $name
+    cd $JOBS_DIR/$name
+    # Run Flexpart-IFS
+    bash job
+done
 
-cd ${workdir}
-./upload_s3.py --directory ${workdir}/sandbox/output
-
-
-# Additionally move results to mounted NFS
-mv ${workdir}/sandbox ${workdir}/flexpart_output/$(date '+%d%m%y%H%M%S')
+# Upload output files of Flexpart-IFS to S3 bucket.
+python3.11 -m flexpart_ifs_utils upload \
+    --directory $JOBS_DIR \
+    --input ${JOBS_DIR}/data \
+    --site $RELEASE_SITE_NAME
