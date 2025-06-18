33d5c4e0c2baa862cfa8bebb4334afc318b449aa
Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>
2025-06-18 11:20:38 +0200

    Bump patch level.

--- src/FLEXPART.f90
+++ src/FLEXPART.f90
@@ -61,7 +61,7 @@ program flexpart
 
   ! FLEXPART version string
   flexversion_major = '10' ! Major version number, also used for species file names
-  flexversion='Version '//trim(flexversion_major)//'.4.5 (2025-05-21)'
+  flexversion='Version '//trim(flexversion_major)//'.4.6 (2025-06-18)'
   verbosity=0
 
   ! Read the pathnames where input/output files are stored
