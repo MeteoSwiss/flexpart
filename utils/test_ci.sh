ccae5ec3385e2a9a5c253bf40ce66ecf5bd25a89
Nestor Tarin Burriel <nestor.tarinburriel@meteoswiss.ch>
2026-01-27 16:27:46 +0100

    DT-276 Execute in CSCS the tests with coverage only

--- utils/test_ci.sh
+++ utils/test_ci.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
+# TODO DT-276 make container CLI in mchbuild configurable (i.e. support sarus)
 run_tests_with_coverage() {
   python -m coverage run --source /scratch/flexpart_ifs_utils/ --data-file test_reports/.coverage --module pytest --capture=tee-sys -vvv -rA --junitxml=test_reports/junit.xml test/
   python -m coverage xml --data-file test_reports/.coverage -o test_reports/coverage.xml
 }
 
-run_pylint() {
-  python -m pylint --output-format=parseable --exit-zero /scratch/flexpart_ifs_utils/ | tee test_reports/pylint.log
-}
-
-
-run_mypy() {
-  mypy -p flexpart_ifs_utils | grep error | tee test_reports/mypy.log
-}
-
 run_ci_tools() {
-  run_tests_with_coverage && run_pylint && run_mypy
+  run_tests_with_coverage
 }
